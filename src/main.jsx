50e0f30fbc6293b5655671521a663ade8de6cdfc
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-11 11:39:23 +0600

    furniture section added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import { Provider } from 'react-redux'
 import store from './app/store'
 import App from './App'
 
+// const style = {
+//   // fontFamily: 'Cookie, cursive'
+//   // font-family: 'Cookie', cursive;
+//   fontFamily: 'Poppins, sans-serif'
+// };
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='max-w-screen-xl mx-auto'>
--- src/pages/Home/FurnitureView/FurnitureView.jsx
+++ src/pages/Home/FurnitureView/FurnitureView.jsx
@@ -1,15 +1,21 @@
 import furniture from '../../../assets/images/furniture.jpg';
 
 const FurnitureView = () => {
+    const style = {
+        fontFamily: 'Cookie, cursive'
+        // font-family: 'Cookie', cursive;
+        // font-family: 'Poppins', sans-serif;
+    };
+
     return (
         <div className='mb-16 relative'>
             <div className=' w-100%  '>
                 <img className='rounded-xl h-full w-full ' src={furniture} alt="alexa-light" />
             </div>
-            <div style={{ color: "#A29781" }} className='absolute top-52 left-1/2 z-index-2'>
-                <p>Special offer on</p>
-                <h3 className="italic font-semibold text-lg">Furniture</h3>
-                <h2 className='text-2xl font-semibold'>Check it out...</h2>
+            <div style={{ color: "#A29781" }} className='absolute top-52 left-1/4 z-index-2'>
+                <p className='text-end'>Special offer on</p>
+                <h3 style={style} className="italic font-semibold text-8xl">Furniture</h3>
+                <h2 className='text-lg font-semibold text-center'>Check it out...</h2>
             </div>
         </div>
     );
