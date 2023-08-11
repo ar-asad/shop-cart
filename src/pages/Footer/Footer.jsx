09a6046d7acdda97f729db2ae3ab6bbaebf74386
arasad1 <asaduzzamanaj07@gmail.com>
2023-08-11 12:23:15 +0600

    footer implemented

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -2,8 +2,16 @@
 
 const Footer = () => {
     return (
-        <div>
-
+        <div style={{ backgroundColor: "#404040" }}>
+            <div className="w-11/12 mx-auto text-gray-200 flex justify-between p-8 items-center">
+                <h4 className="text-2xl"><span className="font-bold">shop</span>Cart</h4>
+                <ul className="flex gap-4">
+                    <li className="list-nones">About us</li>
+                    <li className="list-nones">Contact</li>
+                    <li className="list-nones">Help</li>
+                </ul>
+                <p>English</p>
+            </div>
         </div>
     );
 };
